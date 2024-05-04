88fdd4e5089e12ec4c8384ae383c0331d63fb29f
21 Tech Enterprise <>
2024-05-04 15:27:07 +0700

    done

--- app/components/HomeNew/TopCategory.tsx
+++ app/components/HomeNew/TopCategory.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import Slider from "react-slick";
 import Image from 'next/image';
 
-
 const settings = {
     dots: true,
     infinite: true,
@@ -36,7 +35,6 @@ const settings = {
     ]
 };
 
-
 const TopCategory = ({ categoty = [] }) => {
     return (
         <>
@@ -60,17 +58,25 @@ const TopCategory = ({ categoty = [] }) => {
 function CategoriesItem({ title = '' }) {
     return (
         <>
-            <div className="w-[180px] h-[231px] max-w-[200px] rounded-md bg-[#EEF2F6] py-[25px] px-[38px] text-center items-center justify-center">
-                <div className="bg-white rounded-full h-[100px] w-[100px] flex items-center justify-center text-center p-3">
+            <div className="w-[180px] h-[231px] max-w-[200px] rounded-md bg-[#EEF2F6] py-[10px] px-[10px] text-center items-center justify-center">
+            <Image
+                        src={`/วิชา/${title}.png`}
+                        width={300}
+                        height={500}
+                        alt=""
+                        className="object-contain "
+                    />
+                {/* <div className="bg-white rounded-full flex items-center justify-center text-center p-3">
                     <Image
-                        src={'/brand5.png'}
+                        src={`/วิชา/${title}.png`}
                         width={100}
                         height={30}
                         alt=""
                         className="object-contain "
                     />
-                </div>
-                <div className="text-black text-[16px] font-bold mt-[13px] font-Anuphan">{title}</div>
+                    
+                </div> */}
+                <div className="text-black text-[16px] font-Poppins font-bold mt-[13px]">{title}</div>
                 {/* <div className=" text-xs mt-2 text-[#4f547b] font-normal">1 Course</div> */}
             </div>
         </>
